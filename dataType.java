3b6276b1b6c1be4cfb0606d328a19b655973f586
Shreepad Desai <shreepaddesai2@gmail.com>
2026-05-21 13:12:08 +0530

    Data Types

---
+++ dataType.java
@@ -0,0 +1,19 @@
+public class dataType {
+    public static void main(String[] args) {
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        double myDoubleNum = 19.99;  // Double-precision floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+
+        System.out.println("Integer: " + myNum);
+        System.out.println("Float: " + myFloatNum);
+        System.out.println("Double: " + myDoubleNum);
+        System.out.println("Character: " + myLetter);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("String: " + myText);
+
+        System.out.println("Complete Code");
+    }
+}
